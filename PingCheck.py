e5727c5ca8fed540d702bbbe3b3c298a5da4ec8c
ColbyGPorter <111389109+ColbyGPorter@users.noreply.github.com>
2026-02-12 19:36:10 -0500

    Add packet loss reporting

--- PingCheck.py
+++ PingCheck.py
@@ -1,13 +1,18 @@
 import pythonping
 from pythonping import ping
 
-IpList = ['1.1.1.1', '8.8.8.8']
+IpList = ['1.1.1.1', '10.10.10.10']
 
 for DeviceIP in IpList:
     #Remove/Add verbose=True to hide/show all ping responses
     #Example --- response_list = ping(DeviceIP, count=5, verbose=True)
-    response_list = ping(DeviceIP, count=5, verbose=True)
-    if response_list.success:
-        print(DeviceIP + " is Alive - pythonping")
+    response_list = ping(DeviceIP, count=5, timeout=1)
+    if response_list.packet_loss > 0:
+        loss_rate = response_list.packet_loss * 100
+        loss_rate_str = str(loss_rate)
+        # print(loss_rate_str)
+        print(DeviceIP + " is experiencing " + loss_rate_str + "% packet loss.")
+        print("")
     else:
-        print(DeviceIP + " is Dead - pythonping")
+        print(DeviceIP + " is online.")
+        print("")
